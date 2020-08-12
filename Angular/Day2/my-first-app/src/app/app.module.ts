5b2cf7c8ee29d32495b8ddfa8629d94df06709dd
sbinjv@gmail.com <Java@#19682>
2020-08-12 14:59:15 +0530

    Angular-Day2
    Angular-Day2

---
+++ Angular/Day2/my-first-app/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {FooterComponent} from './footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent,FooterComponent]
+})
+export class AppModule { }
