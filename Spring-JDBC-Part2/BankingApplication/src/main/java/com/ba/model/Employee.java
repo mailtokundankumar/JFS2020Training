2bf33323d0a2810be071bbb94885a9c8bc0a1873
sbinjv@gmail.com <Java@#19682>
2020-09-17 13:44:36 +0530

    Day10-SpringJDBC-Part2
    Day10-SpringJDBC-Part2

---
+++ Spring-JDBC-Part2/BankingApplication/src/main/java/com/ba/model/Employee.java
@@ -0,0 +1,50 @@
+package com.ba.model;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private float empSalary;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(int empId, String empName, float empSalary) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSalary = empSalary;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public float getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(float empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + "]";
+	}
+	
+	
+}
